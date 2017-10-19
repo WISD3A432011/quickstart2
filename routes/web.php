a60af7c7ce26b2cb1b5d21cf3b87c6acf0880768
CHIA-YU TSAI <scorpio1106vt@gm.student.ncut.edu.tw>
2017-10-19 18:17:48 +0800

    建置對應至TaskController的路由

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ Route::auth();
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tasks', 'TaskController@index');
+Route::post('/task', 'TaskController@store');
+Route::delete('/task/{task}', 'TaskController@destroy');
