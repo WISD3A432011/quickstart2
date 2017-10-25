98ad5959836f5f2d919c3bdd0c6add24d6f06ec0
CHIA YU TSAI <scorpio1106vt@gm.student.ncut.edu.tw>
2017-10-25 12:25:35 +0800

    刪除任務

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -71,7 +71,9 @@ class TaskController extends Controller
     {
         $this->authorize('destroy', $task);
 
-        // 刪除該任務
+        $task->delete();
+
+        return redirect('/tasks');
     }
 }
 
